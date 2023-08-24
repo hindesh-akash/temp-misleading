bd75335bdc16c7a017221a2a2611b360f0f7914b
HINDESH AKASH <90022182+hindesh-akash@users.noreply.github.com>
2023-08-25 03:48:06 +0530

    Update 03_sebi.py

--- pages/03_sebi.py
+++ pages/03_sebi.py
@@ -22,6 +22,12 @@ with st.columns(3)[1]:
 
 st.markdown("<h2 style='text-align: center; color: black;'> SEBI PORTAL<hr></h2>", unsafe_allow_html=True)
 
+with st.sidebar:
+    if "api_token" not in st.session_state:
+        st.error("Set the API key first!")
+    else:
+        st.success("API Key already set!")
+    st.markdown("**Select a page above!**")
 
 # --- OPTION MENU --------------------------------------+
 
